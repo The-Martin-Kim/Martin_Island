c958a3e4e93dd919d55cd4b659a8f88b40e77ddb
Martin Kim <iu2407@naver.com>
2024-07-24 02:11:14 +0900

    [feat] Main Page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 /*eslint-disable*/
 import './App.css';
 import CustomNavbar from './components/CustomNavbar';
+import MainPage from "./components/MainPage";
 
 function App() {
     return (
         <div className="App">
             <CustomNavbar/>
+            <MainPage/>
         </div>
     );
 }
---
+++ src/components/MainPage.js
@@ -0,0 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+function MainPage() {
+    return (
+        <Container>
+            <Row>
+                <Col><Intro/></Col>
+                <Col style={{width: "700px", height: "700px"}}><img src="/island.png" alt="island" width={"700px"} height={"700px"}></img></Col>
+            </Row>
+        </Container>
+    );
+}
+
+function Intro() {
+    return (
+        <div style={{textAlign:'left', marginTop:"200px"}}>
+            <h5 className="skranji-regular" style={{color:'#4796FF', fontSize:"32px"}}>Welcome to...</h5>
+            <h1 className="skranji-regular" style={{fontSize:"96px"}}>Martin Island</h1>
+            <p style={{fontSize:"21px"}} className="pretendard-regular">
+                논리와 코딩의 섬,
+                <br/>
+                Martin Island로 여러분을 초대합니다.
+                <br/>
+                이 곳에서 무한의 상상을 마음껏 펼쳐보세요.
+            </p>
+            <Button style={{background:'#4796FF', fontSize:"21px", width:"195px", height: "51px"}} className="pretendard-regular">탐험하러 가기</Button>
+        </div>
+    );
+}
+
+export default MainPage;
