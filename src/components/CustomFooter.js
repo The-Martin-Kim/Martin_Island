fe70a9adb9bfedbaf93d2e5e77a5dce7d434084d
Martin Kim <iu2407@naver.com>
2024-07-24 03:09:14 +0900

    [feat] footer added

---
+++ src/components/CustomFooter.js
@@ -0,0 +1,11 @@
+import Paper from "@mui/material/Paper";
+
+function CustomFooter() {
+    return (
+        <Paper elevation={0} className="pretendard-regular" style={{background: "white", fontSize: "21px", marginBottom: "20px"}}>
+            2024 &copy; Martin Island. All Rights Reserved.
+        </Paper>
+    );
+}
+
+export default CustomFooter;
