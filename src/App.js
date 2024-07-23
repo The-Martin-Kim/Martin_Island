a3bb2fe219d9e14bcbd4eb834830e5fe6341116b
Martin Kim <iu2407@naver.com>
2024-07-24 03:09:40 +0900

    [feat] added components merged

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import CustomNavbar from './components/CustomNavbar';
 import MainPage from "./components/MainPage";
+import CustomFooter from "./components/CustomFooter";
 
 function App() {
     return (
         <div className="App">
             <CustomNavbar/>
             <MainPage/>
+            <CustomFooter/>
         </div>
     );
 }
